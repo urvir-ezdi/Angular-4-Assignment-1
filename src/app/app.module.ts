a0dc07185feb57e732258f1fe50377f7cbf98774
Carlene <carlene.cannon.conner@gmail.com>
2017-07-22 12:32:32 +0100

    Add a warningAlert component using angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AlertsComponent } from './alerts/alerts.component';
 import { SuccessAlertComponent} from './successAlert/successAlert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlertsComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
