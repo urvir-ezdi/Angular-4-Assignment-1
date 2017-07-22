645d184e42cc6296a5721c9db5b135e7cf3a32da
Carlene <carlene.cannon.conner@gmail.com>
2017-07-22 12:30:05 +0100

    Add a successAlert component manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AlertsComponent } from './alerts/alerts.component';
+import { SuccessAlertComponent} from './successAlert/successAlert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlertsComponent
+    AlertsComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/successAlert/successAlert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './successAlert.component.html'
+})
+
+export class SuccessAlertComponent {
+
+}
